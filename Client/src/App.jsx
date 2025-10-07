1990e11457c948f7aa1094c6a84558c57a5b2f35
Suyash Ranchandra Patil <suyapatil5800@gmail.com>
2025-10-07 16:22:33 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 import Login from './Pages/login';
 import Signup from './Pages/signup';
 
-import Empdashboard from './Pages/empdash';
+import Empdashboard from './Pages/DashboardEmp';
 import Dashboard from './Pages/DashboardAdmin';
 import { Route, Routes } from 'react-router-dom';
 
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
   
-        <Route path="/Employee-DashBoard" element={<Empdashboard />} />
+        <Route path="/Employee-DashBoard/*" element={<Empdashboard />} />
         <Route path="/DashboardAdmin/*" element={<Dashboard />} />
         <Route path="/newuser" element={<Signup />} />
       </Routes>
